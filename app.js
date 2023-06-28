440e873847cb616ecf50ec795419c6900eaac5ed
Goldwawe <renhelber@gmail.com>
2023-06-28 18:22:24 +0200

    Added inn Caesar tab

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-function BlastButton() {
-    alert("BOOOOM!")
+function BlastButton1() {
+    alert("1")
+} 
+function BlastButton2() {
+    alert("2")
+} 
+function BlastButton3() {
+    alert("3")
+} 
+function BlastButton4() {
+    alert("4")
+} 
+function BlastButton5() {
+    alert("5")
 } 
